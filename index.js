5d03a51e6a6b0beb4910befaa303b437c5122110
Eric Johnson <0216johnsoneric@gmail.com>
2017-10-05 20:06:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -153,7 +153,14 @@ function moveDodgerLeft() {
    * This function should move DODGER to the left
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
+   window.requestAnimationFrame(function(){
+     const left = positionToInteger(DODGER.style.left)
+     if (left > 0) {
+      DODGER.style.left = `${left - 4}px`;
+    }
+  }
 }
+
 
 function moveDodgerRight() {
   // implement me!
@@ -161,6 +168,9 @@ function moveDodgerRight() {
    * This function should move DODGER to the right
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
+   window.requestAnimationFrame(function(){
+     
+   })
 }
 
 /**
